1f9a44289d40c7a21e16ebe34fbfbd62f5ccbaa1
DanaLooschelders <dana.looschelders@uni-muenster.de>
2022-02-24 10:13:36 +0100

    added tidy way of loading all data files

--- load_data.R
+++ load_data.R
@@ -3,7 +3,7 @@
 # is in csv file format. This implies that the first row for the Norunda data
 # contains the headers for the columns.
 # Also note that the name for Hyytiala file path has been changed such that it
-# does not contain the Swedish ä.
+# does not contain the Swedish ?.
 
 library(tidyverse)
 library(readxl)
@@ -39,3 +39,22 @@ names(N_2001_30) <- N_headers_30[1, ]
 names(N_1997_day) <- N_headers_day[1, ]
 names(N_1999_day) <- N_headers_day[1, ]
 names(N_2001_day) <- N_headers_day[1, ]
+
+#short way load data 
+
+#by using the encoding UTF8 you can also include the swedish Ã¤ --> even though R does not particulary like it
+#you can save a file with UTF8 encoding and also open a file by: file --> reopen with encoding
+library(readxl)
+path="C:/00_Dana/Uni/3. Mastersemester (Erasmus)/Ecosystem Modelling/Major_Project/Database"
+setwd(path) #set wd to path 
+#read in all csv data 
+files_list=list.files(pattern ="\\.csv", recursive = T) #first get all files ending in csv in the working directory and all subfolders
+#loop through all the files in the folders and assign name
+for (i in 1:length(files_list)) assign(files_list[i], 
+         read.csv(files_list[i], header = T, na.strings = c("-9999", "NA", "NAN"), #read csv file with header
+                  stringsAsFactors = F))
+#read in all xlsx file
+files_list=list.files(pattern ="\\.xlsx", recursive = T)
+#loop through all the files in the folders and assign name 
+for (i in 1:length(files_list)) assign(files_list[i], 
+         read_xlsx(path=files_list[i], trim_ws = T)) #read files
