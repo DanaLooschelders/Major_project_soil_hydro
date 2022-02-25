831d84884ca2f9f3aea35f055da2ce195e7b9554
DanaLooschelders <dana.looschelders@uni-muenster.de>
2022-02-25 12:02:12 +0100

    add check for prec

--- data_check.R
+++ data_check.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+####check Evapotranspiration####
 # check if negative ET occurs during winter
 neg_ET_winter <- function(month, daily_ET, counter) {
   rows <- length(daily_ET)
@@ -14,3 +15,9 @@ neg_ET_winter <- function(month, daily_ET, counter) {
 neg_ET_not_winter_1999_day <- neg_ET_winter(H_1999_day$Month, H_1999_day$Evapotr,0)
 neg_ET_not_winter_2000_day <- neg_ET_winter(H_2000_day$Month, H_2000_day$Evapotr,0)
 neg_ET_not_winter_2001_day <- neg_ET_winter(H_2001_day$Month, H_2001_day$Evapotr,0)
+
+####check precipitation####
+#check negative and too high values
+any(Hyytiala1999day$Prec<0|Hyytiala1999day$Prec>100)
+any(Hyytiala2000day$Prec<0|Hyytiala2000day$Prec>100)
+any(Hyytiala2001day$Prec<0|Hyytiala2001day$Prec>100)
