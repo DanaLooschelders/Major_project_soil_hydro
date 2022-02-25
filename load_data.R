9145b076fb5c0aa0aab9bfa8bac431935ce1322c
DanaLooschelders <dana.looschelders@uni-muenster.de>
2022-02-25 11:25:08 +0100

    tidy names

--- load_data.R
+++ load_data.R
@@ -49,12 +49,19 @@ path="C:/00_Dana/Uni/3. Mastersemester (Erasmus)/Ecosystem Modelling/Major_Proje
 setwd(path) #set wd to path 
 #read in all csv data 
 files_list=list.files(pattern ="\\.csv", recursive = T) #first get all files ending in csv in the working directory and all subfolders
+#function edit names of files list
+TidyNames<-function(files){
+  files<-sub(".*/", "", files) #remove the first part with the file path
+  files<-sub(pattern = "älä", "ala", files) ##sub the special characters
+  files<-tools::file_path_sans_ext(files) #remove file extension
+}
+
 #loop through all the files in the folders and assign name 
-for (i in 1:length(files_list)) assign(sub(".*/", "", files_list[i]), #assign name without copying whole path
+for (i in 1:length(files_list)) assign(TidyNames(files_list[i]), #assign tidy names
          read.csv(files_list[i], header = T, na.strings = c("-9999", "NA", "NAN"), #read csv file with header
                   stringsAsFactors = F))
 #read in all xlsx file
 files_list=list.files(pattern ="\\.xlsx", recursive = T)
 #loop through all the files in the folders and assign name 
-for (i in 1:length(files_list)) assign(sub(".*/", "", files_list[i] ), #assign name without copying whole path
+for (i in 1:length(files_list)) assign(TidyNames(files_list[i]), #assign tidy name
          read_xlsx(path=files_list[i], trim_ws = T)) #read files
