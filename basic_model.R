b2eeb01627ab823927c24bf2514dd6346b3b290e
DanaLooschelders <dana.looschelders@uni-muenster.de>
2022-02-25 13:40:46 +0100

    test basic model

--- basic_model.R
+++ basic_model.R
@@ -43,19 +43,31 @@ swc_N_1999day <- calc_swc(N_1999_day$Prec, N_1999_day$Evapotr, max_swc_N, k)
 # groundwater recharge with soil water 𝜃 as state variable
 # ∆𝜃 =𝑃−𝐸−𝑄−𝑅
 # Q = 𝜃k(𝜃/𝜃max)^2
-function(P, ET, init_swc, max_swc, k) {
-  swc_change<-data.frame("swc"=rep(NA, length(P))) #create an output dataframe for the changing soil water
-  swc_change[1]<-init_swc #initial soil water content
-  swc_sum<-data.frame("swc"=rep(NA, length(P))) #output for total soil water content
-  for(i in 2:length(swc_change)){ #soil water content from day 2
-    temp_Q<-swc_change[i-1]*k*(swc_change[i-1]/max_swc)^2 #calculate value for Q
-    if(max_swc < P[i] - temp_Q - ET[i]) # water available for swc is less than max_swc
+calc_swc<-function(P, ET, init_swc, max_swc, k) {
+  swc<-data.frame("change"=rep(NA, length(P))) #output dataframe for the changing soil water
+  swc$sum<-rep(NA, length(P)) #output for total soil water content
+  swc$sum[1]<-init_swc #initial soil water content
+  swc$change[1]<-0
+  swc$runoff<-rep(NA, length(P)) #output for excess soil water (runoff)
+  for(i in 2:length(swc$sum)){ #soil water content from day 2
+    temp_Q<-swc$sum[i-1]*k*(swc$sum[i-1]/max_swc)^2 #calculate value for Q
+    if(max_swc > P[i] - temp_Q - ET[i]) # water available for swc is less than max_swc
     {
-     swc_change[i]<- P[i] - temp_Q - ET[i] #calculate change in soil water
-     swc_sum[i]<-swc_sum[i-1]+swc_change[i] #add change to sum
+     swc$change[i]<- P[i] - temp_Q - ET[i] #calculate change in soil water
+     swc$sum[i]<-swc$sum[i-1]+swc$change[i] #add change to sum
     }else{ #if water available for swc is more or equal to than max swc
-      
+      swc$change[i]<-0
+      swc$sum[i]<-swc$sum[i-1]
+      swc$runoff<-P[i] - temp_Q - ET[i] - max_swc
     }
   
   }
-}
+return(swc)}
+
+#test model
+swc<-calc_swc(P=Hyytiala1999day$Prec, ET=Hyytiala1999day$Evapotr, 
+               init_swc=0, max_swc=33.5, k=0.5)
+#plot results
+plot(swc$change, type="l") 
+plot(swc$sum, type="l")
+abline(h = 33.5, col="red")
