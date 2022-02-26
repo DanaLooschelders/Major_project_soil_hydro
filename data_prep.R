f5412c77ff9ecad5181f88a15239dd77772039ff
DanaLooschelders <dana.looschelders@uni-muenster.de>
2022-02-26 15:00:17 +0100

    tidy up dates

--- data_prep.R
+++ data_prep.R
@@ -5,16 +5,22 @@
 library(tidyverse)
 #tidy up environment
 #write the three years into one dataframe for 30mins
-Norunda_all_30<-rbind(Norunda1997, Norunda1999, Norunda2001)
+Norunda_all_30<-rbind(Norunda1997, Norunda1999, Norunda2001) #for Norunda
 rm(Norunda1997, Norunda1999, Norunda2001) #remove
-Hyytiala_all_30<-rbind(Hyytiala1999, Hyytiala2000, Hyytiala2001)
+Hyytiala_all_30<-rbind(Hyytiala1999, Hyytiala2000, Hyytiala2001)#for Hyytiala
 rm(Hyytiala1999, Hyytiala2000, Hyytiala2001)#remove
 
 #write the three years into one dataframe for daily data
-Norunda_all_day<-rbind(Norunda1997day, Norunda1999day, Norunda2001day)
+Norunda_all_day<-rbind(Norunda1997day, Norunda1999day, Norunda2001day) #for Norunda
 rm(Norunda1997day, Norunda1999day, Norunda2001day) #remove
-Hyytiala_all_day<-rbind(Hyytiala1999day, Hyytiala2000day, Hyytiala2001day)
+#the dates seems to be wrong for Hyytiala -> correct it
+Hyytiala1999day$Year<-1999 #change from 2000 to 1999
+Hyytiala2001day$Year<-2001 #change from 2000 to 2001
+Hyytiala_all_day<-rbind(Hyytiala1999day, Hyytiala2000day, Hyytiala2001day) #for Hyytiala
 rm(Hyytiala1999day, Hyytiala2000day, Hyytiala2001day) #remove
+#create proper date column for Hyytiala
+Hyytiala_all_day$date<-paste(Hyytiala_all_day$Year, Hyytiala_all_day$Month, Hyytiala_all_day$Day) #combine columns for date
+Hyytiala_all_day$date<-strptime(Hyytiala_all_day$date, format="%Y %m %d") #convert date to proper form
 
 #select certain dataframes in environment
 dfs<-mget(ls(pattern = "30"), .GlobalEnv)
