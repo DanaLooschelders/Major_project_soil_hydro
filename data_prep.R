88d298d280fa9349fbda21988203a45b3ceac361
DanaLooschelders <dana.looschelders@uni-muenster.de>
2022-03-04 16:23:02 +0100

    sum up halfhourly fluxes to days

--- data_prep.R
+++ data_prep.R
@@ -21,6 +21,11 @@ rm(Hyytiala1999day, Hyytiala2000day, Hyytiala2001day) #remove
 #create proper date column for Hyytiala
 Hyytiala_all_day$date<-paste(Hyytiala_all_day$Year, Hyytiala_all_day$Month, Hyytiala_all_day$Day) #combine columns for date
 Hyytiala_all_day$date<-strptime(Hyytiala_all_day$date, format="%Y %m %d") #convert date to proper form
+#create proper date column for Norunds
+Norunda_all_day$date<-paste(Norunda_all_day$Year, Norunda_all_day$Month, Norunda_all_day$Day) #combine columns for date
+Norunda_all_day$date<-strptime(Norunda_all_day$date, format="%Y %m %d") #convert date to proper form
+Norunda_all_30$date<-paste(Norunda_all_30$Year, Norunda_all_30$Month, Norunda_all_30$Day) #combine columns for date
+Norunda_all_30$date<-strptime(Norunda_all_30$date, format="%Y %m %d") #convert date to proper form
 
 #select certain dataframes in environment
 dfs<-mget(ls(pattern = "30"), .GlobalEnv)
@@ -29,4 +34,9 @@ for(i in 1:length(dfs)){ dfs[[i]]$Evapotr<-(dfs[[i]]$Evapotr* 10^-3 * 18.02)/100
 list2env(dfs ,.GlobalEnv) #unlist again in environment
 rm(dfs) #remove temp list
 
+#sum the half hourly SWC20 data
+daily_SWC<-Norunda_all_30 %>%
+  group_by(date) %>%
+  summarize(mean = mean(SWC20))
+Norunda_all_day$SWC20<-daily_SWC
 
